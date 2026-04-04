3e07b2f728762c9c496baf9cace70aa49c32ca0e
Leonid Romanov <leonid.romanov@acronis.com>
2026-04-04 13:15:38 +0300

    style: update icon size classes in TagSmallWithIcons component

--- packages/demos/src/tag/TagSmallWithIcons.tsx
+++ packages/demos/src/tag/TagSmallWithIcons.tsx
@@ -10,22 +10,22 @@ import {
 export function TagSmallWithIcons() {
   return (
     <div className="flex flex-wrap gap-3">
-      <Tag variant="success" size="small" icon={<CheckCircleIcon className="h-3 w-3" />}>
+      <Tag variant="success" size="small" icon={<CheckCircleIcon className="size-3" />}>
         Active
       </Tag>
-      <Tag variant="info" size="small" icon={<InfoIcon className="h-3 w-3" />}>
+      <Tag variant="info" size="small" icon={<InfoIcon className="size-3" />}>
         Info
       </Tag>
-      <Tag variant="warning" size="small" icon={<WarningCircleIcon className="h-3 w-3" />}>
+      <Tag variant="warning" size="small" icon={<WarningCircleIcon className="size-3" />}>
         Warning
       </Tag>
-      <Tag variant="critical" size="small" icon={<ExclamationCircleIcon className="h-3 w-3" />}>
+      <Tag variant="critical" size="small" icon={<ExclamationCircleIcon className="size-3" />}>
         Critical
       </Tag>
-      <Tag variant="danger" size="small" icon={<TimesCircleIcon className="h-3 w-3" />}>
+      <Tag variant="danger" size="small" icon={<TimesCircleIcon className="size-3" />}>
         Error
       </Tag>
-      <Tag variant="neutral" size="small" icon={<CircleIcon className="h-3 w-3" />}>
+      <Tag variant="neutral" size="small" icon={<CircleIcon className="size-3" />}>
         Neutral
       </Tag>
     </div>
